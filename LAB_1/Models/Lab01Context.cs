58a367215146447b1246832fd53744125b048209
usuario <usuario@DESKTOP-D1ISP1P>
2024-04-22 23:17:09 -0400

    Agregar archivos de proyecto.

---
+++ LAB_1/Models/Lab01Context.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace LAB_1.Models;
+
+public partial class Lab01Context : DbContext
+{
+    public Lab01Context()
+    {
+    }
+
+    public Lab01Context(DbContextOptions<Lab01Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        if (!optionsBuilder.IsConfigured) { 
+        }
+            }
+       
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(200)")
+                .HasColumnName("id");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(40)
+                .HasColumnName("correo");
+            entity.Property(e => e.FechaRegistro)
+                .HasMaxLength(6)
+                .HasColumnName("fecha_registro");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Password)
+                .HasMaxLength(20)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ LAB_1/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB_1.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public DateTime FechaRegistro { get; set; }
+}
